263d89b1bd496201b9c39905d5ecb1954f97cbd0
ojasvi <the.ojasvi.vashisht@gmail.com>
2025-12-23 17:32:51 +0530

    corrections

--- server.js
+++ server.js
@@ -1,44 +1,55 @@
-const express =  require("express")
+const express = require("express")
 const app = express()
-app.set('view engine','ejs')
+app.set("view engine", "ejs")
 app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-const Document = require('./models/document')
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost/pastebin")
+const Document = require("./models/Document")
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/wastebin")
 
-app.get("/",(req,res)=>{
-    const code = `welcome to watseBin!
-     use to share file text`
-res.render("code-display",{code,language:"plaintext"})
+app.get("/", (req, res) => {
+  const code = `Welcome to WasteBin!
+
+Use the commands in the top right corner
+to create a new file to share with others.`
+
+  res.render("code-display", { code, language: "plaintext" })
 })
 
-app.get('/new',(req,res)=>{
-    res.render("new")
+app.get("/new", (req, res) => {
+  res.render("new")
 })
 
-app.post('/save',async(req,res)=>{
-const value = req.body.value
-try {
-    const document = await document.create({value})
+app.post("/save", async (req, res) => {
+  const value = req.body.value
+  try {
+    const document = await Document.create({ value })
     res.redirect(`/${document.id}`)
-} catch(e){
-    res.render("new",{value})
-}
-console.log(value)
+  } catch (e) {
+    res.render("new", { value })
+  }
 })
 
-app.get('/:id',async(req,res)=>{
-    const id = req.params.id
-    try{
-        const document = await document.findById(id)
-        red.render('code-display',{code: document.value,id})
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+    res.render("new", { value: document.value })
+  } catch (e) {
+    res.redirect(`/${id}`)
+  }
+})
 
-    }
-    catch(e){
-        res.redirect("/")
-    }
+app.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render("code-display", { code: document.value, id })
+  } catch (e) {
+    res.redirect("/")
+  }
 })
 
 app.listen(3000)
